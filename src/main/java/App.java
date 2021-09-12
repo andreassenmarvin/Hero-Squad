2a5a51e3e31b0db531c7d76d447eaa4f73d4c15d
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-12 22:16:04 +0300

    Add home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,23 +5,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.*;
-
 public class App {
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567;
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String[] args) {
+
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
 
-            return new ModelAndView(model, "templates/index.hbs");
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
