b9eb701a47bd2efc7f217afbe917c4e69d069e2f
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-12 20:17:57 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,7 +21,7 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
 
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "templates/index.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
